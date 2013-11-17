b30943ee4d84fc1aa0e51cb4190f135c5f713e55
Steve Kondik <shade@chemlab.org>
2013-11-17 18:01:31 +0100

    Add options to better control video preview size
    That patch needs to be merged before : https://gerrit.omnirom.org/#/c/2468/
    
    Credit to: Steve Kondik
    
    PatchSet: correct logic to do not break other devices
    
    Change-Id: Idc98ba6326b7c548507cd62f95e64e60ec95b1ab
    
    Conflicts:
    	res/values/config.xml
    	src/com/android/camera/VideoModule.java

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -906,14 +906,17 @@ public class VideoModule implements CameraModule,
         }
         mParameters = mCameraDevice.getParameters();
         if (mParameters.getSupportedVideoSizes() == null ||
-                isHFREnabled(mProfile.videoFrameWidth, mProfile.videoFrameHeight)) {
+                isHFREnabled(mProfile.videoFrameWidth, mProfile.videoFrameHeight)) || 
+                       (!mActivity.getResources().getBoolean(
+                        R.bool.usePreferredPreviewSizeForEffects))) {
             mDesiredPreviewWidth = mProfile.videoFrameWidth;
             mDesiredPreviewHeight = mProfile.videoFrameHeight;
         } else { // Driver supports separates outputs for preview and video.
             List<Size> sizes = mParameters.getSupportedPreviewSizes();
             Size preferred = mParameters.getPreferredPreviewSizeForVideo();
-            if (preferred == null) {
-                    preferred = sizes.get(0);
+            if (mActivity.getResources().getBoolean(
+                    R.bool.ignorePreferredPreviewSizeForVideo) || preferred == null) {
+                preferred = sizes.get(0);
             }
             int product = preferred.width * preferred.height;
             Iterator<Size> it = sizes.iterator();
